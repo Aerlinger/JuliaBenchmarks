9f124fd50ecafc37f4c55b1206cb60cf95728a3d
Anthony Erlinger <aerlinger@gmail.com>
2014-06-21 12:54:51 -0400

    Remove unnecessary code in julia_remote_execution_context_spec

--- spec/services/julia_remote_execution_context_spec.rb
+++ spec/services/julia_remote_execution_context_spec.rb
@@ -1,15 +1,4 @@
 require 'rails_helper'
 
 describe JuliaRemoteExecutionContext do
-  attr_reader :code
-
-  def initialize(code)
-    @code = code
-  end
-
-  class << self
-    def remote_call(code)
-      http_url = ENV['JULIA_SERVER_URL']
-    end
-  end
 end
