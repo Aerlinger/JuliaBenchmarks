2d5bbfd11784e964fec2bf0ffc020255b68210d0
Anthony Erlinger <aerlinger@gmail.com>
2014-06-21 10:06:05 -0400

    Update Capybara to 2.3

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 group :test do
-  gem "rspec-rails", group: [:test, :development]
+  gem "rspec-rails"
   gem "capybara"
 end
 
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,7 +5,27 @@ describe "Home Page" do
     visit "/"
   end
 
-  it "has corect text" do
+  it "has correct text" do
     expect(page).to have_text("Julia Benchmarks")
+  end
+
+  describe "Login", pending: true do
+    before do
+      click_link "login"
+    end
+
+    it "redirects to the login page" do
+      expect(page.status_code).to eq(200)
+    end
+  end
+
+  describe "Create Account", pending: true do
+    before do
+      click_link "Create account"
+    end
+
+    it "redirects to the create account page" do
+      expect(page.status_code).to eq(200)
+    end
   end
 end
