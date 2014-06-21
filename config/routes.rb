638050a225bee7cb459a302dcf75a27732fa90e5
Anthony Erlinger <aerlinger@gmail.com>
2014-06-21 13:03:56 -0400

    Add routes/controllers and views for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "static_pages#home"
 
+
+  get "/users/:id"      => "users#show"
+  get "/users/edit/:id" => "users#edit"
+  put "/users/edit/:id" => "users#update"
+
+  get "/snippets/:id"       => "snippets#show"
+  get "/snippets/:id/edit"  => "snippets#edit"
+  post "/snippets/:id/fork" => "snippets#fork"
+  delete "/snippets/:id"    => "snippets#delete"
+
   get "/demo" => "repl#demo"
   post "/run" => "repl#run"
 end
