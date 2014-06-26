11a5991895c2a797877fa566229266bfc220f278
Anthony Erlinger <aerlinger@gmail.com>
2014-06-25 21:48:20 -0400

    Successful AJAX response with data fetched from Julia server.

--- app/controllers/repl_controller.rb
+++ app/controllers/repl_controller.rb
@@ -1,13 +1,14 @@
 class ReplController < ApplicationController
   respond_to :js, only: :run
   before_filter :save_snippet_to_current_user
+  layout false, only: :run
 
   def demo
     @snippet = Snippet.new
   end
 
   def run
-    code = params[:repl].fetch(:code)
+    code = params["snippet"].fetch("code")
 
     # This call blocks until return
     response_hash = JuliaRemoteExecutionContext.safe_eval(code)
@@ -15,6 +16,10 @@ class ReplController < ApplicationController
     @output    = response_hash.fetch(:output)
     @result    = response_hash.fetch(:result)
     @exception = response_hash.fetch(:exception)
+
+    respond_to do |format|
+      format.js #{ layout: false }
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "static_pages#home"
+  root to: "repl#demo"
 
   get "/users/:id"      => "users#show"
   get "/users/edit/:id" => "users#edit"
