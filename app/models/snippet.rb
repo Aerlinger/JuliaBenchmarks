8975e2f62019e66b4e7e00b7db042cfa3de8d998
Anthony Erlinger <aerlinger@gmail.com>
2014-07-05 14:22:28 -0700

    Update annotations & add find_by_public token to user model

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -8,10 +8,12 @@
 #  updated_at        :datetime
 #  parent_snippet_id :integer
 #  user_id           :integer
+#  public_token      :string(255)
 #
 # Indexes
 #
 #  index_snippets_on_parent_snippet_id  (parent_snippet_id)
+#  index_snippets_on_public_token       (public_token) UNIQUE
 #  index_snippets_on_user_id            (user_id)
 #
 
@@ -26,6 +28,10 @@ class Snippet < ActiveRecord::Base
   belongs_to :parent_snippet, foreign_key: :parent_snippet_id
 
   validates_presence_of :code
+
+  def self.find_by_token(token)
+    find_by!(public_token: token)
+  end
 
   def fork!
     Snippet.create! code: code, parent_snippet_id: id
--- spec/factories/snippets.rb
+++ spec/factories/snippets.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: snippets
+#
+#  id                :integer          not null, primary key
+#  code              :text
+#  created_at        :datetime
+#  updated_at        :datetime
+#  parent_snippet_id :integer
+#  user_id           :integer
+#  public_token      :string(255)
+#
+# Indexes
+#
+#  index_snippets_on_parent_snippet_id  (parent_snippet_id)
+#  index_snippets_on_public_token       (public_token) UNIQUE
+#  index_snippets_on_user_id            (user_id)
+#
+
 FactoryGirl.define do
   factory :snippet do
     code 'println("Hello, world")'
