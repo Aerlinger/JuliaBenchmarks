06965c605b5b1bc47e9e4a158051075c06a1c0e5
Anthony Erlinger <aerlinger@gmail.com>
2014-07-05 14:32:35 -0700

    Add guest attribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705211701) do
+ActiveRecord::Schema.define(version: 20140705213159) do
 
   create_table "benchmark_results", force: true do |t|
     t.string   "category"
@@ -50,16 +50,17 @@ ActiveRecord::Schema.define(version: 20140705211701) do
     t.string   "score"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "guest",                  default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
